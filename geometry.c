b66e8e597e6ddb60dd3025844e1fcafd961d414b
zzzdew <eldos.crazy@gmail.com>
2022-04-05 20:22:51 +0700

    fix some problems

--- geometry.c
+++ geometry.c
@@ -1,10 +1,11 @@
 #include <ctype.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-bool finding_bracket(char* UKAZAT)
+bool finding_bracket(char* UKAZAT) //поиск скобки
 {
     int flag = 0;
     while (*UKAZAT != 10) {
@@ -19,7 +20,7 @@ bool finding_bracket(char* UKAZAT)
     return true;
 }
 
-bool finding_bracket2(char* UKAZAT)
+bool finding_bracket2(char* UKAZAT) //поиск второй скобки
 {
     int flag = 0;
     while (*UKAZAT != 10) {
@@ -34,7 +35,7 @@ bool finding_bracket2(char* UKAZAT)
     return true;
 }
 
-bool finding_comma(char* UKAZAT)
+bool finding_comma(char* UKAZAT) //поиск запятой
 {
     int flag = 0;
     while (*UKAZAT != 10) {
@@ -49,34 +50,80 @@ bool finding_comma(char* UKAZAT)
     return true;
 }
 
-bool correct(char* Circle, char* str, int N)
-{
-    int g = 6;
-    if (strncmp(Circle, str, g) != 0)
-        printf("error: Check the spelling of the word  -circle- ");
+bool correct(char* Circle, char* UKAZAT, int N)
+{ //проверка правильности вводимых данных
+
+    int g = 6, check = 0;
+    if (strncmp(Circle, UKAZAT, g) == 0) {
+        check++;
+        if (finding_bracket(UKAZAT) == true)
+            check++;
+        if (finding_comma(UKAZAT) == true)
+            check++;
+        if (finding_bracket2(UKAZAT) == true)
+            check++;
+    } else {
+        printf("error: Check the spelling of the command");
+    }
+    if (check == 4)
+        return true;
+    if (check != 4)
+        return false;
     return true;
 }
-
+void translation_universal(char* UKAZAT, double* x1)
+{ //переводим х из текста в число
+    char* point;
+    double c;
+    int i = 0;
+    while (isdigit(*UKAZAT) == 0) {
+        UKAZAT++;
+        if (isdigit(*UKAZAT) != 0) {
+            c = strtod(UKAZAT, &point);
+            x1[i] = c;
+            i++;
+            UKAZAT = point;
+            if (i == 3)
+                break;
+        } else {
+            printf("expected '<double>'\n");
+            system("pause");
+            break;
+        }
+    }
+}
+void calculation(double* A)
+{
+    float pi = 3.1415;
+    float S;
+    float P;
+    P = 2 * pi * A[2];
+    S = pi * pow(A[2], 2);
+    printf("Perimetr = %f\n", P);
+    printf("Area = %f\n", S);
+}
 int main()
 {
     int N = 50;
-    double x, y, radius;
+    double A[4];
     char str[N];
     char* UKAZAT = str;
     char* UKAZAT2 = str;
-    int i = 0;
     char Circle[] = {"circle"};
-    int circlen = strlen(Circle);
 
-    fgets(str, N, stdin);
-    if (isalpha(*UKAZAT) != 0) {
+    fgets(str, N, stdin); //ввод данных
+    if (isalpha(*UKAZAT) != 0) { //передвигаем указатель на первую скобку
         while (isalpha(*UKAZAT2) != 0)
             UKAZAT2++;
     }
     if (correct(Circle, str, N) == true) {
+        //если проверка прошла успешно, передвигаем первый указатель
+        //на первую скобку
         UKAZAT = UKAZAT2;
-    } else
-        return 0;
+        printf("correct\n");
+    }
+    translation_universal(UKAZAT, A);
+    calculation(A);
     system("pause");
     return 0;
 }
