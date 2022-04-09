e58c7e7cb5784875d2c39dd826cc2ad62464b904
zzzdew <eldos.crazy@gmail.com>
2022-04-09 14:18:59 +0700

    I split the file into separate parts, create a library

---
+++ geometry.h
@@ -0,0 +1,14 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#pragma once
+
+bool finding_bracket(char* UKAZAT);
+bool finding_bracket2(char* UKAZAT);
+bool finding_comma(char* UKAZAT);
+bool correct(char* Circle, char* UKAZAT, int N);
+void translation_universal(char* UKAZAT, double* x1);
+void calculation(double* A);
